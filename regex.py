4dfc81b6b71f281155b9aac9b5c3a4088c71095c
Madhur Malhotra <malhotra.madhur15@gmail.com>
2016-12-20 23:53:39 -0600

    got program to pass in filename, open file, and return text

--- regex.py
+++ regex.py
@@ -1,8 +1,15 @@
 import re
 
 def clean(text):
-
+    remove = text
+    return remove
 
 
 if __name__ == '__main__':
-    clean()
+    import sys
+    filename = sys.argv[1]
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+        source = open(filename).read()
+        clean = clean(source)
+        print clean
