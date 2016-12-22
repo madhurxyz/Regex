82f4818ec644f99f62e38a012ced6e595770ce95
Madhur Malhotra <malhotra.madhur15@gmail.com>
2016-12-21 18:32:25 -0600

    incorporating re.compile() method

--- regex.py
+++ regex.py
@@ -1,12 +1,13 @@
 import re
 
 def clean(text):
-    remove = re.sub(r'(?:(?:(^[\w]+[\'\-]?[\w]*))|(^(?:[\w]+[\'\-]?[\w]*[\'\-]?)+)|(^(?:[\$]?[\w]+[\'\-]?[\w]*)+))', '', text).split()
+    remove = re.sub(r'(?:(?:(^[\w]+[\'\-]?[\w]*))|(^(?:[\w]+[\'\-]?[\w]*[\'\-]?)+)|(^(?:[\$]?[\w]+[\'\-]?[\w]*)+))', '', text)
+    sentence = re.compile("[^.](.*?)[!.;?]")
     # remove = re.sub(r'[^\w\'?\w\$\s]', '', text).split()
     # remove = re.sub(r'(^[\w\'?\w]*[\$\s])', '', text).split()
 
 
-    return remove
+    return sentence
 
 
 if __name__ == '__main__':
