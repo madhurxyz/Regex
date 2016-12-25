5b6323186fc7b5e2b69e03267f4256122374735d
Madhur Malhotra <malhotra.madhur15@gmail.com>
2016-12-24 23:24:19 -0500

    avoiding regex for now, started self-writing function to remove punctuation

--- regex.py
+++ regex.py
@@ -3,15 +3,24 @@ import re
 def clean(text):
     # remove = re.sub(r'[^\w\'?\w\$\s]', '', text).split()
     # remove = re.sub(r'(^[\w\'?\w]*[\$\s])', '', text).split()
-    remove = re.sub(r'(?:(?:(^[\w]+[\'\-]?[\w]*))|(^(?:[\w]+[\'\-]?[\w]*[\'\-]?)+)|(^(?:[\$]?[\w]+[\'\-]?[\w]*)+))', '', text)
-    regex = re.compile("[^.](.*?)[!.;?]")
-    matches = regex.findall(text)
-    sentences = []
-    for match in matches:
-        sentences.append(match)
+    # (?:(?:'([\wÀ-ÿ]+[\'\-]?[\wÀ-ÿ]*)')|((?:[\wÀ-ÿ]+[\'\-]?[\wÀ-ÿ]*[\'\-]?)+)|((?:['\$]?[\wÀ-ÿ]+[\'\-]?[\wÀ-ÿ]*)+))
+    #
+    # [^\wÀ-ÿ\'?\wÀ-ÿ\$\s]
+    # remove = re.sub(r'(?:(?:(^[\w]+[\'\-]?[\w]*))|(^(?:[\w]+[\'\-]?[\w]*[\'\-]?)+)|(^(?:[\$]?[\w]+[\'\-]?[\w]*)+))', '', text)
+    # regex = re.compile("[^.](.*?)[!.;?]")
+    # matches = regex.findall(text)
+    # sentences = []
+    # for match in matches:
+    #     sentences.append(match)
+    #
+    # return sentences
+    punctuations = '!()-[]{};:"\,<>./?@#$%^&*_~\x80\x98\x99\x94'
+    no_punctuation = ''
+    for character in word_string:
+        no_punctuation = "wirte function here"
+    return no_punctuation
 
-    return sentences
-
+    
 if __name__ == '__main__':
     import sys
     filename = sys.argv[1]
